088c8a451c6f270ce5a3133c142ce7732b9581e6
linshire <1127384500@qq.com>
2022-11-05 11:04:20 +0800

    修复测试空间内存可能会不对齐的可能

--- sourcecode/memset_utest.c
+++ sourcecode/memset_utest.c
@@ -15,15 +15,19 @@
 
 
 /*TEST_SIZE_SPACE * sizeof(rt_base_t) chars*/
-
 #define TEST_SIZE_SPACE 4
 
-ALIGN(RT_ALIGN_SIZE)
+#if TEST_SIZE_SPACE < 2
+#error "TEST_SIZE_SPACE should be bigger than 2"
+#endif  /* TEST_SIZE_SPACE < 28 */
 
 static void test_memset_base(void)
 {
     rt_uint8_t i = 0;
     rt_uint8_t result = 0;
+
+    ALIGN(RT_ALIGN_SIZE)
+
     rt_base_t test_space[TEST_SIZE_SPACE];
 
     /*init test_space with filling 0*/
@@ -58,25 +62,6 @@ static void test_memset_base(void)
 
 }
 
-// static void test_memset_uint8(void)
-// {
-//     rt_uint8_t i = 0;
-//     rt_uint8_t result = 0;
-//     rt_base_t test_space[TEST_SIZE_SPACE] = {0};
-//     /* fill with 1 */
-//     rt_memset((rt_uint8_t *)test_space + 1, 0xff, sizeof(rt_base_t));
-
-//     result = *((rt_uint8_t *)test_space + 1);
-
-//     for(i = 0; i < sizeof(rt_base_t); i++)
-//     {
-//     result &= *((rt_uint8_t *)test_space + 2 + i);
-//     }
-//     result &= 0xff;
-
-//     uassert_int_equal(result, 0xff);
-// }
-
 
 static rt_err_t utest_tc_init(void)
 {
@@ -92,4 +77,4 @@ static void testcase(void)
 {
     UTEST_UNIT_RUN(test_memset_base);
 }
-UTEST_TC_EXPORT(testcase, "NOT ALIGN(RT_ALIGN_SIZE) test", utest_tc_init, utest_tc_cleanup, 10);
+UTEST_TC_EXPORT(testcase, "NOT ALIGN(RT_ALIGN_SIZE) memset test", utest_tc_init, utest_tc_cleanup, 10);
